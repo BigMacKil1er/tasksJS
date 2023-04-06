ddc3de46546f17a934e3539ff4fe3184ece90444
BigmacKiller <varik_varibrus@mail.ru>
2023-04-06 13:45:04 +0300

    пару задач по объектам, таких как калькулятор мини, и аккумулятор хзшный, и проверка есть ли метод в обьекте, что бы не было  ошибки

--- some information about basics of javascript/objects.js
+++ some information about basics of javascript/objects.js
@@ -19,55 +19,150 @@
 // Object.assign(obj, one, tw0, show)
 // obj.showThis()
 
-console.log('==================')
+// console.log('==================')
+//
 
-
-let objWithObj = {
-    brend: 'Bmw',
-    model: 3,
-    parameters: {
-        color: 'blue',
-        power: '300hp',
-        volume: '3.0'
-    },
-    show: function () {
-        console.log(this)
-    }
-}
-let unobj = 'string'
-let thisIsNull = null
+// let objWithObj = {
+//     brend: 'Bmw',
+//     model: 3,
+//     parameters: {
+//         color: 'blue',
+//         power: '300hp',
+//         volume: '3.0'
+//     },
+//     show: function () {
+//         console.log(this)
+//     }
+// }
+// let unobj = 'string'
+// let thisIsNull = null
 
 // simple copy object
 
-function copyObj(obj) {
-    let copy = {}
-    for (const objKey in obj) {
-        if ((typeof obj[objKey]) === 'object') {
-            copy[objKey] = copyObj(obj[objKey])
-        } else {
-            copy[objKey] = obj[objKey]
-        }
-
-    }
-    return copy
-
-}
+// function copyObj(obj) {
+//     let copy = {}
+//     for (const objKey in obj) {
+//         if ((typeof obj[objKey]) === 'object') {
+//             copy[objKey] = copyObj(obj[objKey])
+//         } else {
+//             copy[objKey] = obj[objKey]
+//         }
+//
+//     }
+//     return copy
+//
+// }
 
 // console.log(typeof obj)
-console.log('first')
-console.log(copyObj(objWithObj))
+// console.log('first')
+// console.log(copyObj(objWithObj))
 // console.log(copyObj(unobj))
 // copyObj(objWithObj)
 // copyObj(unobj)
 // copyObj(null)
-console.log('===================')
-let copy = copyObj(objWithObj)
-console.log('===================')
-console.log('second')
-copy.show()
-console.log('===================')
-copy.parameters.color = 'red'
+// console.log('===================')
+// let copy = copyObj(objWithObj)
+// console.log('===================')
+// console.log('second')
+// copy.show()
+// console.log('===================')
+// copy.parameters.color = 'red'
+//
+// console.log('third and fourth')
+// objWithObj.show()
+// copy.show()
 
-console.log('third and fourth')
-objWithObj.show()
-copy.show()
+// простой калькулятор но ток плюс и умножение
+// const calc = {
+//     a: 0,
+//     b: 0,
+//     read(num1, num2) {
+//         this.a = num1
+//         this.b = num2
+//     },
+//     sum() {
+//         return this.a + this.b
+//     },
+//     mul() {
+//         return this.a * this.b
+//     }
+// }
+//
+// console.log('=================')
+//
+// calc.read(2, 3)
+// console.log(calc.sum())
+// console.log(calc.mul())
+
+// Здесь тема про конструкторы
+// let Person = function (name, age, sex) {
+//     this.name = name
+//     this.age = age
+//     this.sex = sex
+// }
+//
+//
+// let artyom = new Person('artyom', 28, 'male')
+// let arsik = new Person('Arsen', 28, "female")
+//
+// console.log(artyom)
+// console.log(arsik)
+
+// ==========================  небольшие задачки ============================
+//-------Первая задачка. сдеалть что бы две функции при сравнении были равны-----------
+
+// let obj = {}
+// function A(name) { return obj }
+// function B(name) { return obj }
+//
+// let a = new A();
+// let b = new B();
+// console.log(a == b)
+// alert( a == b ); // true
+
+// -----------калькулятор на конструкторе--------------
+// function Calc() {
+//     this.read = function (num1, num2) {
+//         this.num1 = num1
+//         this.num2 = num2
+//     }
+//     this.sum = function () {
+//         return this.num1 + this.num2
+//     }
+//     this.mul = function () {
+//         return this.num1 * this.num2
+//     }
+// }
+//
+// let twoDigit = new Calc()
+// twoDigit.read(3, 5)
+// console.log(twoDigit.mul())
+// console.log(twoDigit.sum())
+
+
+//----------------аккумулятор какой то------------------
+
+// function Accumulator(num) {
+//     this.num = num
+//     this.read = function () {
+//         this.num += num
+//     }
+//     this.value = function () {
+//         return this.num
+//     }
+// }
+// let acb = new Accumulator(1)
+//
+// acb.read()
+// console.log(acb.value())
+//
+// acb.read()
+// console.log(acb.value())
+//
+// acb.read()
+// console.log(acb.value())
+
+
+let user = {}
+
+console.log(user.adress?.street)
