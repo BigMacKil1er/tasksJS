fa4ee7d144f4dd61febbe891fcea6fa267123f74
BigmacKiller <varik_varibrus@mail.ru>
2023-04-08 15:06:08 +0300

    Методы масивов такие, как push, pop, unshift, shift, delete, splice, slice, concat, переборы масивов for..in, for..of, forEach

--- some information about basics of javascript/arrey(Массивы).js
+++ some information about basics of javascript/arrey(Массивы).js
@@ -28,3 +28,56 @@ function mass(arr) {
     console.log('5', arr)
 }
 mass(arrTask)
+// ============all methods=============
+let someArrey = ['apple']
+let someArrey2 = new Array('apple')
+
+someArrey.push('Pineapple')          // [ 'apple', 'Pineapple' ]
+someArrey.pop('Pineapple')           // [ 'apple' ]
+someArrey.unshift('Pineapple') // [ 'Pineapple', 'apple' ]
+someArrey.shift('Pineapple')        // [ 'apple' ]
+
+someArrey2.push('Pineapple', 'Pear', 'Peach')
+//---циклы---
+for (const string of someArrey2) {
+    console.log(string)
+    // apple
+    // Pineapple
+    // Pear
+    // Peach
+}
+for (const someArrey2Key in someArrey2) {
+    console.log(someArrey2Key)
+    // 0
+    // 1
+    // 2
+    // 3
+    console.log(someArrey2[someArrey2Key])
+    // apple
+    // Pineapple
+    // Pear
+    // Peach
+}
+
+console.log(someArrey2[1])               // Pineapple
+console.log(someArrey2[-1])              // undefined
+console.log(someArrey2.at(1))      // Pineapple
+console.log(someArrey2.at(-1))     // Peach
+
+delete someArrey2[0]                     // [ <1 empty item>, 'Pineapple', 'Pear', 'Peach' ] удалил значение но место занимает
+console.log(someArrey2.length)           // 4 Длина такая же
+someArrey2.splice(0, 1)   // начиная с индекса 0, удаляет первый
+someArrey2.splice(0, 2, 'mandarin', "plum") //начина с индекса 0 удаляет первые два, затем на их место добавляет два новых значания [ 'mandarin', 'plum', 'Peach' ]
+let smAr2 = someArrey2.splice(0, 2, 'mandarin', "plum") // Возвращает два удаленных
+someArrey2.splice(2, 0, 'grapefruit')
+
+let a = someArrey2.slice(0, 3) // Создает копию массива с, по и возвращает его
+console.log(someArrey2)
+const obj = {name: 'Naira'}
+let b = a.concat(someArrey2, ['one', 'two'], obj) // копирует масивы в масив и можно добавить не только масивы но и объекты и примитивы
+console.log(b)
+
+b.forEach((item, index, array) => { // Перебор фор ичем со стрелочной ф-нкцией
+    console.log(`Получается у ${array} имеется индекс ${index} и значение ${item}`)
+})
+b.forEach(console.log) // выводит все сразу в консоль
