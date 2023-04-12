c6efe1f0007e9d851dc52e88df0ff929353e9ed0
BigmacKiller <varik_varibrus@mail.ru>
2023-04-12 16:14:08 +0300

    and added easy problem solution

--- some information about basics of javascript/Object.keys, values, entries.js
+++ some information about basics of javascript/Object.keys, values, entries.js
@@ -1,6 +1,11 @@
 // оОбъекты мы можем перебрать с помощью Object.keys, values, entries.js
 // по сути она превращает объект в массив
 
+// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+//
+//     Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+//
+//     Если объект salaries пуст, то результат должен быть 0.
 
 let salaries = {
     "John": 100,
@@ -22,3 +27,17 @@ function sumSalaries(obj) {
 
 
 console.log(sumSalaries(salaries))
+
+// Напишите функцию count(obj), которая возвращает количество свойств объекта:
+
+
+function count(obj) {
+    return Object.entries(obj).length
+}
+let user = {
+    name: 'John',
+    age: 30,
+    legs: 2
+};
+
+console.log(count(user))
