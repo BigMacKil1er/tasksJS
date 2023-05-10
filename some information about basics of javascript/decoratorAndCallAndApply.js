ebd2c4e76bc0a21be43c119fa75ae0f1b9bb2b29
BigmacKiller <varik_varibrus@mail.ru>
2023-05-10 14:41:02 +0300

    solution problem

--- some information about basics of javascript/decoratorAndCallAndApply.js
+++ some information about basics of javascript/decoratorAndCallAndApply.js
@@ -37,3 +37,34 @@ const obj = {
 obj.someFunction = cacheDecorator(obj.someFunction)
 console.log(obj.someFunction(5))
 console.log(obj.someFunction(5))
+
+// obout apply
+
+
+
+
+
+//the problems
+
+function spy(func) {
+    function wrapper(...args){
+        wrapper.calls.push(args)
+        return func.apply(this, args)
+    }
+    wrapper.calls = []
+    return wrapper
+
+
+}
+function work(a, b) {
+    console.log( a + b ); // произвольная функция или метод
+}
+
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+work(5, 6);// 11
+for (let args of work.calls) {
+    console.log( 'call: ' + args.join() ); // "call:1,2", "call:4,5"
+}
