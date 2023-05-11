c5b2649c1f8b055a89268ea18efab7058cfdc516
BigmacKiller <varik_varibrus@mail.ru>
2023-05-11 19:44:28 +0300

    Little add about prototype

--- some information about basics of javascript/prototype.js
+++ some information about basics of javascript/prototype.js
@@ -1,53 +1,72 @@
 // __proto__
 // Object.getPrototypeOf()
 
-function Cat(name, color) {
-    this.name = name
-    this.color = color
+// function Cat(name, color) {
+//     this.name = name
+//     this.color = color
+// }
+//
+// Cat.prototype.voice = function () {
+//     console.log(`Cat ${this.name} says myau`)
+// }
+// const cat = new Cat('Kot', 'white')
+//
+// console.log('Спросим какой прототип у cat?:', Cat.prototype)
+//
+// console.log('===================')
+// console.log('Ниже вызываем этот прототип функцию')
+// cat.voice()
+//
+// console.log('===================')
+// //-----=-=-====
+//
+// function Person() {}
+//
+// Person.prototype.legs = 2
+// Person.prototype.skin = 'white'
+//
+// const person = new Person()
+// person.name = 'Tom'
+//
+// console.log('skin' in person) //true
+// console.log(person.legs)      // 2
+// console.log(person.name)      // Tom
+// console.log(person)           // Person { name: 'Vladlen' }
+//
+// console.log('===================')
+//
+// console.log(person.hasOwnProperty('name'))
+// console.log(person.hasOwnProperty('skin'))
+//
+// console.log('===================')
+// //Object.create()
+//
+// const proto = {year: 2023}
+// const myYear = Object.create(proto)
+//
+// console.log(myYear.year)
+// console.log(myYear.hasOwnProperty('year'))
+// console.log(myYear.__proto__ === proto)
+//
+// proto.year = 2024
+//
+// console.log(myYear.year)
+
+let user = {
+    name: 'Jackie',
+    surname: 'Chan',
+    set fullName(value){
+        [this.name, this.surname] = value.split(' ')
+    },
+    get fullName(){
+        return `${this.name} ${this.surname}`
+    }
 }
-
-Cat.prototype.voice = function () {
-    console.log(`Cat ${this.name} says myau`)
+let admin = {
+    __proto__: user,
+    isAdmin: true
 }
-const cat = new Cat('Kot', 'white')
-
-console.log('Спросим какой прототип у cat?:', Cat.prototype)
-
-console.log('===================')
-console.log('Ниже вызываем этот прототип функцию')
-cat.voice()
-
-console.log('===================')
-//-----=-=-====
-
-function Person() {}
-
-Person.prototype.legs = 2
-Person.prototype.skin = 'white'
-
-const person = new Person()
-person.name = 'Tom'
-
-console.log('skin' in person) //true
-console.log(person.legs)      // 2
-console.log(person.name)      // Tom
-console.log(person)           // Person { name: 'Vladlen' }
-
-console.log('===================')
-
-console.log(person.hasOwnProperty('name'))
-console.log(person.hasOwnProperty('skin'))
-
-console.log('===================')
-//Object.create()
-
-const proto = {year: 2023}
-const myYear = Object.create(proto)
-
-console.log(myYear.year)
-console.log(myYear.hasOwnProperty('year'))
-console.log(myYear.__proto__ === proto)
-
-proto.year = 2024
-
-console.log(myYear.year)
+console.log(admin.fullName)
+admin.fullName = 'Jet Li'
+console.log(admin.fullName)
+console.log(user.fullName)
