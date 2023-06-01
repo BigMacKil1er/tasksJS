99d52042f54c2e219141f6b2d24bab46146b9115
BigmacKiller <varik_varibrus@mail.ru>
2023-06-02 01:25:06 +0300

    solved simple problem

--- some information about basics of javascript/proxyAndReflect.js
+++ some information about basics of javascript/proxyAndReflect.js
@@ -24,3 +24,20 @@ try {
 } catch (e) {
     console.log(e.message)
 }
+
+let arr = []
+
+for (let i = 0; i < 10; i++) {
+    arr.push(i)
+}
+
+arr = new Proxy(arr,{
+    get(target, p, receiver) {
+        if (p < 0){
+            p = +p + target.length
+        }
+        return Reflect.get(target, p, receiver)
+    }
+})
+console.log(arr[1])
+console.log(arr[-1])
